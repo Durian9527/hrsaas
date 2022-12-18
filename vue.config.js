26f3b12c8d49175a8f50ce2f1ce866175a32b484
Durian <AbigDurian9527@163.com>
2022-12-18 17:28:22 +0800

    处理响应拦截器

--- vue.config.js
+++ vue.config.js
@@ -35,6 +35,14 @@ module.exports = {
     overlay: {
       warnings: false,
       errors: true
+    },
+    proxy: {
+      // 当我们的本地的请求 有/api的时候，就会代理我们的请求地址向另外一个服务器发出请求
+      '/api': {
+        target: 'http://ihrm.itheima.net/', // 跨域请求的地址 此服务器管理员密码禁止修改
+        // 另外一个服务器地址：http://ihrm-java.itheima.net/
+        changeOrigin: true // 只有这个值为true的情况下 才表示开启跨域
+      }
     }
     // before: require('./mock/mock-server.js')
   },
