11fac218a2672c9bc69df57bfc0495a86c642506
Durian <AbigDurian9527@163.com>
2022-12-21 14:15:52 +0800

    路由与页面

--- src/icons/index.js
+++ src/icons/index.js
@@ -7,3 +7,4 @@ Vue.component('svg-icon', SvgIcon)
 const req = require.context('./svg', false, /\.svg$/)
 const requireAll = requireContext => requireContext.keys().map(requireContext)
 requireAll(req)
+// 以上两行代码是将所有的SVG目录下的所有.svg文件全部引入到项目中
