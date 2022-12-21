66f610539d384b457fe00c1a18a9e0162f1e7383
Durian <AbigDurian9527@163.com>
2022-12-21 10:49:20 +0800

    各路由模块规则初始化

---
+++ src/router/modules/approvals.js
@@ -0,0 +1,14 @@
+// 审批路由规则
+import Layout from '@/layout'
+export default {
+  path: '/approvals',
+  name: 'approvals',
+  component: Layout,
+  children: [{
+    path: '',
+    component: () => import('@/views/approvals'),
+    meta: {
+      title: '审批'
+    }
+  }]
+}
